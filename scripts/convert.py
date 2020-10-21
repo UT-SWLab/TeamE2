8a984d8025fa2090681e745017cc0bf2d7e49745
Edie Zhou <edie198@gmail.com>
2020-10-21 15:41:32 -0500

    added file deletion to file image conversion script

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,10 +1,13 @@
-
 # Pythono3 code to rename multiple  
 # files in a directory or folder 
 # and convert all .jpg to .png
+# Drop this script in the same
+# directory that you want to convert
+# images for
   
 # importing os module 
 import os 
+from PIL import Image
   
 # Function to rename multiple files 
 def main():
@@ -15,10 +18,16 @@ def main():
         elif filename[-3] == '-':
             os.rename(filename, filename[:-3])
 
-  
-# Driver Code 
+        is_jpg = filename[-4:] == '.jpg'
+        png_exists = os.path.exists(filename[:-4]+'.png')
+        if is_jpg and not png_exists:
+            im1 = Image.open(filename)
+            im1.save(filename[:-4]+'.png')
+            os.remove(filename)
+        elif is_jpg and png_exists:
+            os.remove(filename)
+
+
 if __name__ == '__main__': 
-      
-    # Calling main() function 
     main() 
 
