f7824863658c7dd703725919d9d5268095887cad
Kevin Medina <kvnmedina59@gmail.com>
2020-11-15 13:03:10 -0600

    Added instance links to homepage

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ def about():
 
 @app.route('/models_drivers')
 def driver_model():
-    
     query = request.args.get('search', '', type=str).rstrip()
     if query == '':
         driver_list = db.drivers.find()
@@ -49,11 +48,11 @@ def driver_model():
                     driver_list.append(driver)
         else:
             # Search first token in forenames, search other tokens in surnames
-            driver_list = list(db.drivers.find({'forename' :{'$regex' : f'{tokens[0]}?', '$options' : 'i'}}))
+            driver_list = list(db.drivers.find({'forename': {'$regex': f'{tokens[0]}?', '$options': 'i'}}))
             surname_cursors = []
 
             for i in range(1, len(tokens)):
-                surname_cursors.append(db.drivers.find({'surname' :{'$regex' : f'{tokens[i]}?', '$options' : 'i'}}))
+                surname_cursors.append(db.drivers.find({'surname': {'$regex': f'{tokens[i]}?', '$options': 'i'}}))
             for cursor in surname_cursors:
                 for driver in cursor:
                     if driver not in driver_list:
@@ -63,10 +62,10 @@ def driver_model():
     page = page - 1
     drivers = []
     for driver in driver_list:
-        if 'driverId' in driver.keys():      
+        if 'driverId' in driver.keys():
             drivers.append(
-                {'driverId': driver['driverId'], 'driverRef': driver['driverRef'], 
-                'surname': driver['surname'], 'forename': driver['forename'], 'nationality': driver['nationality']})
+                {'driverId': driver['driverId'], 'driverRef': driver['driverRef'],
+                 'surname': driver['surname'], 'forename': driver['forename'], 'nationality': driver['nationality']})
             if 'constructor' not in driver:
                 con = list(db.results.find({'driverId': driver['driverId']}))
                 all_constructors = []
@@ -76,7 +75,7 @@ def driver_model():
                     constructor = {'id': constructorId, 'name': constructorName}
                     if constructor not in all_constructors:
                         all_constructors.append(constructor)
-                all_constructors.reverse() #newest to oldest
+                all_constructors.reverse()  # newest to oldest
                 # print(all_constructors)
                 db.drivers.update_one({'_id': driver['_id']}, {'$set': {'constructor': all_constructors[0]}})
                 db.drivers.update_one({'_id': driver['_id']}, {'$set': {'all_constructors': all_constructors}})
@@ -84,7 +83,7 @@ def driver_model():
                 # TODO: Consider adding logging, print statements clutter up the terminal
                 # print('found constructor for driver: ' + str(driver['driverId']))
                 drivers[-1].update({'constructor': driver['constructor']})
-            drivers[-1].update({'link': 'drivers?id='+str(driver['driverId'])})
+            drivers[-1].update({'link': 'drivers?id=' + str(driver['driverId'])})
 
             # Get image
             driver_ref = driver['driverRef']
@@ -101,7 +100,6 @@ def driver_model():
 
 @app.route('/models_constructors')
 def constructor_model():
-    
     # get constructors from search
     constructor_list = search('name', db.constructors)
 
@@ -112,13 +110,13 @@ def constructor_model():
     for constructor in constructor_list:
         if 'constructorId' in constructor.keys():
             constructors.append(
-                {'constructorId': constructor['constructorId'], 'constructorRef': constructor['constructorRef'], 
-                'name': constructor['name'], 'nationality': constructor['nationality']}) 
+                {'constructorId': constructor['constructorId'], 'constructorRef': constructor['constructorRef'],
+                 'name': constructor['name'], 'nationality': constructor['nationality']})
             if 'topDriverName' in constructor.keys():
                 constructors[-1].update({'top_driver': constructor['topDriverName']})
             else:
                 constructors[-1].update({'top_driver': 'N/A'})
-            constructors[-1].update({'link': 'constructors?id='+str(constructor['constructorId'])})
+            constructors[-1].update({'link': 'constructors?id=' + str(constructor['constructorId'])})
 
             # Get image
             constructor_ref = constructor['constructorRef']
@@ -127,14 +125,13 @@ def constructor_model():
                 img_path = NO_IMG
             constructors[-1].update({'imgpath': img_path})
     per_page = 20
-    pages = int(len(constructors)/per_page)
-    constructors = constructors[page*per_page: page*per_page+per_page]
+    pages = int(len(constructors) / per_page)
+    constructors = constructors[page * per_page: page * per_page + per_page]
     return render_template('constructors-model.html', constructors=constructors, pages=pages, page=page)
 
 
 @app.route('/models_circuits')
 def circuit_model():
-
     # get constructors from search
     circuit_list = search('name', db.circuits)
 
@@ -144,21 +141,22 @@ def circuit_model():
     for circuit in circuit_list:
         if 'circuitId' in circuit.keys():
             circuits.append(
-                {'circuitId': circuit['circuitId'], 'circuitRef': circuit['circuitRef'], 
-                'name': circuit['name'], 'location': circuit['location'], 'country': circuit['country']})
+                {'circuitId': circuit['circuitId'], 'circuitRef': circuit['circuitRef'],
+                 'name': circuit['name'], 'location': circuit['location'], 'country': circuit['country']})
             if 'most_recent_race' not in circuit.keys():
                 mrr = db.races.find({'circuitId': circuit['circuitId']}).sort([('date', -1)])
                 mrr = list(mrr)
                 if len(mrr) > 0:
                     mrr = mrr[0]
                     print(mrr['name'] + ' ' + mrr['date'])
-                    db.circuits.update_one({'_id': circuit['_id']}, {'$set': {'most_recent_race': mrr['name'] + ' ' + mrr['date']}})
+                    db.circuits.update_one({'_id': circuit['_id']},
+                                           {'$set': {'most_recent_race': mrr['name'] + ' ' + mrr['date']}})
                 else:
                     db.circuits.update_one({'_id': circuit['_id']}, {'$set': {'most_recent_race': 'N/A'}})
             else:
                 print('found most recent race for circuit: ' + str(circuit['circuitId']))
                 circuits[-1].update({'most_recent_race': circuit['most_recent_race']})
-            circuits[-1].update({'link': 'circuits?id='+str(circuit['circuitId'])})
+            circuits[-1].update({'link': 'circuits?id=' + str(circuit['circuitId'])})
 
             # Get image
             circuit_ref = circuit['circuitRef']
@@ -335,14 +333,14 @@ def home():
     today = date.today()
     currentMonth = str(today).split("-")[1]
     currentMonthName = today.strftime('%B')
-    currentDay =  str(today).split("-")[2]
-    currentYear =  str(today).split("-")[0]
+    currentDay = str(today).split("-")[2]
+    currentYear = str(today).split("-")[0]
 
-    recentRaces = db.races.find({'year' : int(currentYear)}).limit(5)
+    recentRaces = db.races.find({'year': int(currentYear)}).limit(5)
     recentRaces = list(recentRaces)
 
-    regDate = "....-" + currentMonth + "-.." 
-    drivers = db.drivers.find({'dob' : {'$regex' : regDate}}).limit(20)
+    regDate = "....-" + currentMonth + "-.."
+    drivers = db.drivers.find({'dob': {'$regex': regDate}}).limit(20)
     drivers = list(drivers)
     currentMonthDrivers = []
     index = 0
@@ -350,19 +348,20 @@ def home():
         driver_ref = driver['driverRef']
         name = driver['forename'] + "  " + driver['surname']
         image_path = f'images/drivers/{driver_ref}.png'
-        tempDict = {'driver_ref' : driver_ref, 'image_path' : image_path, 'name' : name , 'id' :driver['driverId']}
+        if not os.path.exists(f'./static/{image_path}'):
+            image_path = NO_IMG
+        tempDict = {'driver_ref': driver_ref, 'image_path': image_path, 'name': name, 'id': driver['driverId']}
         currentMonthDrivers.append(tempDict)
-        index+=1
+        index += 1
 
-
-    thisSeasonResults = db.results.find({'year' : int(currentYear)})
-    seasonDriverPoints =  {}
+    thisSeasonResults = db.results.find({'year': int(currentYear)})
+    seasonDriverPoints = {}
     seasonConstructorPoints = {}
     for result in thisSeasonResults:
         driver = result['driverId']
         driverPoints = result['points']
         driverName = result['driverName']
-        
+
         constructor = result['constructorId']
         constructPoints = result['points']
         constructorName = result['constructorName']
@@ -370,17 +369,17 @@ def home():
             currentPoints = seasonDriverPoints[driver][1]
             seasonDriverPoints[driver][1] = currentPoints + driverPoints
         else:
-            seasonDriverPoints[driver] = [driverName , driverPoints]
+            seasonDriverPoints[driver] = [driverName, driverPoints, driver]
 
         if constructor in seasonConstructorPoints:
             currentPoints = seasonConstructorPoints[constructor][1]
             seasonConstructorPoints[constructor][1] = currentPoints + constructPoints
         else:
-            seasonConstructorPoints[constructor] = [constructorName, constructPoints]
-    
+            seasonConstructorPoints[constructor] = [constructorName, constructPoints, constructor]
+
     sortedDriverSeasonLeaders = []
     for key in sorted(seasonDriverPoints.keys(), key=lambda k: seasonDriverPoints[k][1], reverse=True):
-       sortedDriverSeasonLeaders.append(seasonDriverPoints[key])
+        sortedDriverSeasonLeaders.append(seasonDriverPoints[key])
 
     sortedConstructorSeasonLeaders = []
 
@@ -389,7 +388,7 @@ def home():
 
     recentRaces = recentRaces[:5]
     sortedConstructorSeasonLeaders = sortedConstructorSeasonLeaders[:5]
-    sortedDriverSeasonLeaders =  sortedDriverSeasonLeaders[:5]
+    sortedDriverSeasonLeaders = sortedDriverSeasonLeaders[:5]
 
     drivers = db.drivers.aggregate([{'$sample': {'size': 10}}])
     popularDrivers = []
@@ -397,24 +396,28 @@ def home():
         driver_ref = driver['driverRef']
         name = driver['forename'] + "  " + driver['surname']
         image_path = f'images/drivers/{driver_ref}.png'
-        tempDict = {'driver_ref' : driver_ref, 'image_path' : image_path, 'name' : name, 'id' : driver['driverId']}
+        if not os.path.exists(f'./static/{image_path}'):
+            image_path = NO_IMG
+        tempDict = {'driver_ref': driver_ref, 'image_path': image_path, 'name': name, 'id': driver['driverId']}
         popularDrivers.append(tempDict)
 
-    circuits = db.circuits.aggregate([{'$sample' : {'size': 10}}])
+    circuits = db.circuits.aggregate([{'$sample': {'size': 10}}])
     popularCircuits = []
     for circuit in circuits:
         if 'circuitRef' in circuit.keys():
             circuit_ref = circuit['circuitRef']
             name = circuit['name']
             image_path = f'images/circuits/{circuit_ref}.png'
-            tempDict = {'circuit_ref' : circuit_ref, 'name' : name, 'image_path' : image_path, 'id' : circuit['circuitId']}
+            if not os.path.exists(f'./static/{image_path}'):
+                image_path = NO_IMG
+            tempDict = {'circuit_ref': circuit_ref, 'name': name, 'image_path': image_path, 'id': circuit['circuitId']}
             popularCircuits.append(tempDict)
 
-
-    return render_template('home.html' , recentRaces = recentRaces , monthDrivers = currentMonthDrivers, 
-                            driverSeasonStandings = sortedDriverSeasonLeaders, 
-                            constructorSeasonStandings = sortedConstructorSeasonLeaders,
-                            year = currentYear, monthName = currentMonthName, popularCircuits = popularCircuits, popularDrivers=popularDrivers)
+    return render_template('home.html', recentRaces=recentRaces, monthDrivers=currentMonthDrivers,
+                           driverSeasonStandings=sortedDriverSeasonLeaders,
+                           constructorSeasonStandings=sortedConstructorSeasonLeaders,
+                           year=currentYear, monthName=currentMonthName, popularCircuits=popularCircuits,
+                           popularDrivers=popularDrivers)
 
 
 def search(field, collection):
@@ -433,7 +436,7 @@ def search(field, collection):
     if query == '':
         return collection.find()
     else:
-        return collection.find({field :{'$regex' : f'{query}?', '$options' : 'i'}})
+        return collection.find({field: {'$regex': f'{query}?', '$options': 'i'}})
 
 
 if __name__ == '__main__':
